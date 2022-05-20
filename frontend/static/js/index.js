0b94f38e46d9c4751498021f34106b4bbecf6c56
jorgeruizm94 <97487049+jorgeruizm94@users.noreply.github.com>
2022-05-19 22:44:44 -0700

    v2

--- frontend/static/js/index.js
+++
@@ -1,9 +0,0 @@
-import {getCountries} from './countries.js'
-
-const countries = getCountries();
-
-console.log(countries);
-
-$(".theme-switch").on("click", () => {
-    $("body").toggleClass("light-theme");
-  });
